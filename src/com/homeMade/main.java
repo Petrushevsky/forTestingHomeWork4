3ddf992eeb8645ad68f27dfe98b4af5fe3bed368
Eugene <odessa.eugene@gmail.com>
2022-04-04 12:26:47 +0300

    Текст песни "Упала шляпа" Упала шляпа упала на пол

---
+++ src/com/homeMade/main.java
@@ -0,0 +1,8 @@
+package com.homeMade;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Упала шляпа");
+        System.out.println("упала на пол");
+    }
+}
